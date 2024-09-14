625cfc27f1696dce7cf45b3fa1324f7638281612
AS KASHMARY <88844672+as-kashmary@users.noreply.github.com>
2024-09-14 23:53:29 +0600

    update

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const collection = require("./config");
 const Task = require("./task");
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const session = require('express-session');
 
 const app = express();
